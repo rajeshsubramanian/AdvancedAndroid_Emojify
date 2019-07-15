32758528d70a07015e504e4ccbc33eb790ecbda1
Rajesh Subramanian <rsubramanian@resourceammirati.com>
2019-07-15 13:01:50 -0400

    Checking in Exercise 2

---
+++ app/src/main/java/com/example/android/emojify/Emojifier.java
@@ -0,0 +1,14 @@
+package com.example.android.emojify;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.util.Log;
+
+public class Emojifier {
+
+	private static final String LOG_TAG = Emojifier.class.getName();
+
+	public static void detectFaces(Context context, Bitmap bitmap) {
+		Log.d(LOG_TAG, "Number of faces?");
+	}
+}
